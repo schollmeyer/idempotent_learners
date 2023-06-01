a8cc4b9eb13d78aeb7a8ccda8aff05a9ce0f61c8
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2023-06-01 21:49:10 +0200

    Update benchmark_kernel_regression_vs_kernel_svr.R

--- benchmark_kernel_regression_vs_kernel_svr.R
+++ benchmark_kernel_regression_vs_kernel_svr.R
@@ -1,8 +1,8 @@
 idempotent_kernel_regression <- function(x,y,bandwidth,eps=10^-9){
-   K <- gausskernel(X=x,sigma=bandwidth)
-   model <- list(Q=t(K)%*%K + diag(rep(eps,length(x))),obj=-2*t(y)%*%K)
+   kernel_matrix <- gausskernel(X=x,sigma=bandwidth)
+   model <- list(Q=t(kernel_matrix)%*%kernel_matrix + diag(rep(eps,length(x))),obj=-2*t(y)%*%kernel_matrix)
    model$A <- array(1,c(1,length(x)))
-   model$sense <-"<="
+   model$sense <- "<="
    model$rhs <- sum(y)+1000000
    model$lb <- rep(-Inf,length(x))
    model$ub <- rep(Inf,length(x))
@@ -10,10 +10,10 @@ idempotent_kernel_regression <- function(x,y,bandwidth,eps=10^-9){
    while(is.null(solution$x) | solution$status=="SUBOPTIMAL"){
         eps <- eps * 9/8
 	# print(eps)
-	model$Q <- t(K)%*%K +diag(rep(eps,length(x)))	
+	model$Q <- t(kernel_matrix)%*%K +diag(rep(eps,length(x)))	
         solution <- try(gurobi(model,list(outputflag=0)), silent=TRUE)
    }
    # solution <<- solution
    # print(solution$status)
-   return(K%*%solution$x)
+   return(kernel_matrix%*%solution$x)
    }
