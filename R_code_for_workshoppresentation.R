61a637b13310614b9c8d06fc4f4432b7d6968454
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2024-05-02 13:51:02 +0200

    gs

--- R_code_for_workshoppresentation.R
+++ R_code_for_workshoppresentation.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 set.seed(1234567)
-n_sample <- 200
+n_sample <- 75
 sd <- 5
-x <- seq(1,n_sample,1)
+x <- seq(1,200,length.out=75)
 y_true <- 5*sin(x/10)+.1*x
 y <- y_true+ sd * rnorm(n_sample)
 pplot <- ggplot(data=data.frame(x=x,y=y), aes(x=x,y=y))
@@ -49,6 +49,15 @@ pplot  + layer( mapping = NULL,   position = "identity",   stat="identity",geom 
 
 pplot <- ggplot(data=data.frame(x=x,y=y), aes(x=x,y=y))
 pplot  + layer( mapping = NULL,   position = "identity",   stat="identity",geom = "point") +labs(x = "x",y="y") + layer( mapping = NULL,  data=data.frame(x=x,y=y_true), aes(color = "darkgreen",lwd=.8),position = "identity",   stat="identity",   geom = "line") + layer( mapping = NULL,  data=data.frame(x=x,y=classical_result$fitted), aes(color = "black",lwd=.8),position = "identity",   stat="identity",   geom = "line")
+#
+
+
+# Spiky kernel
+x_new <- c(x,x+10^-6,x-10^-6)
+y_new <- c(y,classical_result$fitted,classical_result$fitted)
+o <- order(x_new)
+pplot <- ggplot(data=data.frame(x=x,y=y), aes(x=x,y=y))
+pplot  + layer( mapping = NULL,   position = "identity",   stat="identity",geom = "point") +labs(x = "x",y="y") + layer( mapping = NULL,  data=data.frame(x=x_new[o],y=y_new[o]), aes(color = "black",lwd=.4),position = "identity",   stat="identity",   geom = "line")
 
 #  layer( mapping = NULL,   position = "identity",   stat="identity",geom = "point") +labs(x = "x",y="y")
 #+ layer( mapping = NULL,  data=data.frame(x=x,y=y_true), aes(color = "darkgreen"",lwd=.8),position = "identity",   stat="identity",   geom = "line")
@@ -72,3 +81,22 @@ pplot + layer( aes(color = "black",lwd=.8),mapping = NULL,   data=data.frame(x=l
 
 
 
+# spiky indicator kernel
+
+library(ggplot2)
+set.seed(1234567)
+n_sample <- 75
+sd <- 5
+x <- seq(1,200,length.out=500)
+y_true <- (sin(x/10)+1)/4 +.2
+y <- runif(500) <= y_true
+y <- y*1
+
+
+classical_result <- classical_kernel_regression(x=x,y=y,bandwidth=bw_classical)
+y_fitted <- (classical_result$fitted) >=0.5
+y_fitted=1*y_fitted
+lines(x,classical_result$fitted>=0.5,col="red")
+#y <- y_true+ sd * rnorm(n_sample)
+pplot <- ggplot(data=data.frame(x=x,y=y), aes(x=x,y=y))
+pplot  + layer( mapping = NULL,   position = "identity",   stat="identity",geom = "point") +labs(x = "x",y="y") + layer( mapping = NULL,  data=data.frame(x=x,y=y_fitted), aes(color = "darkblue",lwd=.8),position = "identity",   stat="identity",   geom = "line") + layer( mapping = NULL,   position = "identity",   stat="identity",geom = "point",data=data.frame(x=x,y=y_true)) +labs(x = "x",y="y")
